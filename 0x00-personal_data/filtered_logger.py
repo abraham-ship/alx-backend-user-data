409e6f8e440aed1e802ca7b419be83bcd0efe00f
abraham-ship <abuzsein3@gmail.com>
2024-01-11 06:57:23 +0000

    added type annotation to the function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 '''Regex-ing'''
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     '''Regex-ing'''
     return re.sub(fr'({separator.join(fields)})[^{separator}]*(\
             ?:{separator}|$)', redaction, message)
