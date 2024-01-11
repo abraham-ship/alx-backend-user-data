4c84c94e43299fa177aa747d583d018f06ff4341
abraham-ship <abuzsein3@gmail.com>
2024-01-11 13:20:26 +0000

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 import re
 from typing import List
 import logging
+from logging import StreamHandler
 
 
 class RedactingFormatter(logging.Formatter):
@@ -28,3 +29,21 @@ def filter_datum(fields: List[str], redaction: str,
     '''Regex-ing'''
     return re.sub(fr'({separator.join(fields)})[^{separator}]*(\
             ?:{separator}|$)', redaction, message)
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def get_logger() -> logging.Logger:
+    '''Create logger'''
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+
+    stream_handler = StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    return logger
