4123ce87a03212a1602a87c469298d42b9fcb5dc
abraham-ship <abuzsein3@gmail.com>
2024-01-17 08:31:36 +0000

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -49,8 +49,8 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
         '''returns the User instance based on his email and password'''
-        if user_email is None or not isinstance(user_email, str) or\
-                user_pwd is None or not isinstance(user_pwd, str):
+        if user_email is None or not isinstance(user_email, str)\
+                or user_pwd is None or not isinstance(user_pwd, str):
             return None
 
         user_instance = User.search(user_email)
