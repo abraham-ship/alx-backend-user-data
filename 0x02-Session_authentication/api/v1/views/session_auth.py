9a133b9d6a52a1f229a17f4df3f7c6e3a5d844d7
abraham-ship <abuzsein3@gmail.com>
2024-01-19 10:33:24 +0000

    fixed typing error

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -6,7 +6,7 @@ from os import getenv
 from models.user import User
 
 
-@app_views.route('/auth_session/login' methods=["POST"], strict_slashes=False)
+@app_views.route('/auth_session/login', methods=["POST"], strict_slashes=False)
 def session_login() -> str:
     '''POST /api/v1/auth_session/login
     Return
