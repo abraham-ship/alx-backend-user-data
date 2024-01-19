e687257f23290aed23926466a2366c3a55f733b8
abraham-ship <abuzsein3@gmail.com>
2024-01-19 11:12:41 +0000

    Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -36,3 +36,16 @@ def session_login() -> str:
     response = jsonify(user.to_json())
     response.set_cookie(session_cookie, session_id)
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def session_logout() -> str:
+    '''DELETE /api/v1/auth_session/logout
+    Return
+    - an empty JSON dictionary with the status code 200'''
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
