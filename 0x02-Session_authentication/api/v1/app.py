74898c4c1617440ed7220269af05ef4505038749
abraham-ship <abuzsein3@gmail.com>
2024-01-19 08:44:49 +0000

    fixed indentation

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,8 +38,8 @@ def filter_request() -> None:
             ]
     if auth:
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None:
+            if auth.authorization_header(
+                    request) is None and auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
