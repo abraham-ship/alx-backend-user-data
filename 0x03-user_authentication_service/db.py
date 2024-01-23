bdd474b71741428ed11b6b60be0fecc18c714d4b
abraham-ship <abuzsein3@gmail.com>
2024-01-23 15:06:07 +0000

    db update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy.orm.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
